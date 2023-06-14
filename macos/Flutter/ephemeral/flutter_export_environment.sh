4921f9fbeb1709165e246f1f26f300375cf2bf84
JangJunYeong <wkdwnsdud25@gmail.com>
2023-06-14 13:33:35 +0900

    아이콘 추가

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/seongtaegyeong/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/seongtaegyeong/Desktop/YPass/flutter/git/YPassFlutter"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\YPassFlutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
