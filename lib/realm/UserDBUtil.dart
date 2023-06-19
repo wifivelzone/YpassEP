0b695b7687f94d47ce3babada5911bf93311c9d1
JangJunYeong <wkdwnsdud25@gmail.com>
2023-06-19 14:17:38 +0900

    버전 수정 + 관리자용 코드 추가

--- lib/realm/UserDBUtil.dart
+++ lib/realm/UserDBUtil.dart
@@ -54,7 +54,7 @@ class UserDBUtil {
   List<String> getDong() {
     String addStr = getUser().addr;
     List<String> addrArr = addStr.split("::");
-    if (kDebugMode){
+    if (getUser().phoneNumber == "1"){
       addrArr[0] += "101동 301호"; //test Code
     }
     List<String> split = addrArr[0].split(" ");
@@ -72,7 +72,8 @@ class UserDBUtil {
     String result = "";
     String addStr = getUser().addr;
     List<String> addrArr = addStr.split("::");
-    if (kDebugMode){
+    debugPrint("Phone Number Check : ${getUser().phoneNumber}");
+    if (getUser().phoneNumber == "1"){
       addrArr[0] += "101동 301호"; //test Code
     }
     debugPrint("Addr Array Check : $addrArr");
